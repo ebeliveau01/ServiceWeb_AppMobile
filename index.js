844476f05a357d8eb2cc8fb82aad119684dc5fb2
ebeliveau01 <2238950@carrefour.cegepvicto.ca>
2024-05-09 02:21:55 -0400

    Salut

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT | 3000;
 
-app.use('/api/crud', authentification, require('./src/routes/crud.routes'));
 app.use('/api', require('./src/routes/api.routes'));
+app.use('/api/crud', authentification, require('./src/routes/crud.routes'));
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré ${PORT}`);
