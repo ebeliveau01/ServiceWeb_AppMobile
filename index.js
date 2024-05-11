ef7f0e52d8322f6314471b2a8def522edc8ad318
ebeliveau01 <2238950@carrefour.cegepvicto.ca>
2024-05-11 01:22:51 -0400

    Salut

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 80;
 
 app.use('/api', require('./src/routes/api.routes'));
 app.use('/api/crud', require('./src/routes/crud.routes'));
